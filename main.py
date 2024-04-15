9e2c83a2f863f9acef06eb51f5351c0b76e6df06
Vickiing <victor.lima.silva@hotmail.com>
2024-04-15 09:12:41 -0300

    commit up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import urllib
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import os
