59d6e4780bd954f2d8ba732748388e87b7388895
Vickiing <victor.lima.silva@hotmail.com>
2024-04-17 09:52:53 -0300

    -desktop mod.

--- rpa_desktop.py
+++ rpa_desktop.py
@@ -1,6 +1,37 @@
 import pyautogui as gui
 from time import sleep, time
+import win32clipboard
 from PIL import Image
+from io import BytesIO
+
+def enviar_texto_para_area_de_transferencia(arquivo):
+    with open(arquivo, 'r', encoding='utf-8') as arquivo:
+        texto = arquivo.read()
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+
+    try:
+        win32clipboard.SetClipboardText(texto, win32clipboard.CF_UNICODETEXT)
+
+    except Exception as e:
+        print(f"Erro ao enviar texto para a área de transferência: {e}")
+
+    win32clipboard.CloseClipboard()
+
+
+def captura_imagem():
+    
+
+    path = r"C:\Users\vlsilva\Documents\PYTHON PROJETOS\WApy\WARPA\images\117.jpg"
+    image = Image.open(path)
+    output = BytesIO()
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+    output.close()
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+    win32clipboard.CloseClipboard()
 
 
 
@@ -8,7 +39,7 @@ from PIL import Image
 arquivo = 'numeros_teste.txt'
 
 start_time = time()
-texto = '[Teste de envio de mensagem...]'
+arquivo_texto = r'C:\Users\vlsilva\Documents\PYTHON PROJETOS\WApy\WARPA\mensagem_convidativa.txt'
 
 gui.hotkey('alt', 'tab')
 sleep(2)
@@ -23,15 +54,19 @@ with open(arquivo, "r") as file:
         sleep(1)
         gui.press('backspace', presses=15 ,interval=0.1)
         gui.typewrite(str(numero), interval=0.1)
-        sleep(4)
+        sleep(5)
         gui.click(187,182)
         sleep(4)
+        captura_imagem()
         gui.hotkey('ctrl', 'v')
         sleep(2.5)
         gui.typewrite('oi', interval=0.1)
         gui.hotkey('enter')
         sleep(2.5)
-        gui.typewrite(texto, interval=0.1)
+        #gui.typewrite(texto, interval=0.1)
+        enviar_texto_para_area_de_transferencia(arquivo_texto)
+        gui.hotkey('ctrl', 'v')
+        sleep(2.5)
         gui.press('enter')
         sleep(2)
         #limpar pesquisa e inicia nova conversa
